e86c8cf50c6b186ad891400603479605260fd92c
SuyashMore <suyash.21.more@gmail.com>
2019-01-26 18:17:12 +0530

    Added Mobile 2 Python Communication (Works)

--- controlServer.py
+++ controlServer.py
@@ -1,27 +1,52 @@
 from socket import *
+import threading
+
 
 HOST = ''
-PORT = 21567
+PORT = 21565
 BUFSIZE = 1024
 ADDR = (HOST,PORT)
 
 tcpSerSock = socket(AF_INET, SOCK_STREAM)
 tcpSerSock.bind(ADDR)
-tcpSerSock.listen(5)
+# tcpSerSock.listen(5)
+tcpSerSock.listen(0)
 
-while True:
-        print ('Waiting for connection')
-        tcpCliSock,addr = tcpSerSock.accept()
-        print ('...connected from :', addr)
-        try:
-                while True:
-                        data = ''
-                        data = tcpCliSock.recv(BUFSIZE)
-                        if not data:
-                                break
-                        else:
-                            print("Data Received:",data)
-        except KeyboardInterrupt:
-                pass
 
-tcpSerSock.close();
+
+Sdata = ""
+
+def getInput():
+	while True:
+			# print ('Waiting for connection')
+			tcpCliSock,addr = tcpSerSock.accept()
+			# print ('...connected from :', addr)
+			try:
+					while True:
+							data = ''
+							data = tcpCliSock.recv(BUFSIZE)
+							if not data:
+									break
+							else:
+								print("Data Received:",data)
+								Sdata=data
+			except KeyboardInterrupt:
+					pass
+
+	tcpSerSock.close();
+
+def printData():
+	PData = ""
+	while True:
+		if PData !=Sdata:
+			print(Sdata)
+			PData=Sdata
+		else:
+			pass
+
+
+if __name__=="__main__":
+	t1 = threading.Thread(target=getInput)
+	t2 = threading.Thread(target=printData)
+	t1.start()
+	t2.start()
