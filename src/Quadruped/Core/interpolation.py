6a591c6a26e61401e9dd96b99c8d309f1221e88c
SuyashMore <suyash.21.more@gmail.com>
2019-02-23 20:23:47 +0530

    Added 1d Interpolation Function

--- src/Quadruped/Core/interpolation.py
+++ src/Quadruped/Core/interpolation.py
@@ -1,6 +1,4 @@
 import numpy as np
-# import matplotlib.pyplot as plt
-# from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 import time 
 
 
@@ -9,15 +7,43 @@ Intitial_config = np.array([8,7,-16])
 Final_config = np.array([8,-4,-16])
 
 
+Intitial_config = np.array([-np.pi,0,0])
+Final_config = np.array([np.pi,0,0])
 
 # Velocities dx,ty,dz,....
 Initial_veclocity = np.array([0,0,0])
-Final_veclotiy = np.array([0,3,0])
-
+Final_veclotiy = np.array([0,0,0])
 
 # Time of Path 
 Iniital_time = 0.0
 Final_time = 1
+
+
+def setInterpolate1d(position,velocities,time):
+    global Intitial_config,Final_config,Initial_veclocity,Final_veclotiy,Iniital_time,Final_time,A,B,C,D
+
+    Intitial_config = np.array([position[0],0,0])
+    Final_config = np.array([position[1],0,0])
+
+        # Velocities dx,ty,dz,....
+    Initial_veclocity = np.array([velocities[0],0,0])
+    Final_veclotiy = np.array([velocities[1],0,0])
+
+
+    # Time of Path 
+    Iniital_time = time[0]
+    Final_time = time[1]
+
+    A,B,C,D = getABCD()
+
+def getInterpolate1d(time):
+    return (getConfigAt(time))[0]
+    # return 0
+    pass
+
+
+
+
 
 def getABCD():
     D = Intitial_config
@@ -30,8 +56,8 @@ def getABCD():
 
     return A,B,C,D
 
-
 A,B,C,D = getABCD()
+
 
 def getConfigAt(t):
     t=t*1.0
@@ -47,39 +73,43 @@ def getAcc(t):
     return res
 
 if __name__ == "__main__":
-    pass
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 
-    # input = range(0,11,0.1)
-    # pos = []
-    # vel = []
-    # acc = []
-
-    # sctx = []
-    # scty = []
-
-    # start = time.time()
-    # current = time.time()
-    # while (time.time()-start <=(Final_time-Iniital_time)):
-    #     config = getConfigAt(time.time()-start)
-    #     # ax.scatter(config[0],config[1],config[2])
-    #     time.sleep(1)
-    #     print("Calculating at time:",time.time()-start)
     
-    # print(res)
-    # plt.plot(input,pos)
-    # plt.plot(input,vel)
-    # plt.plot(input,acc)
-    # plt.plot(sctx,scty)
-    # plt.xlabel("X")
-    # plt.xlabel("Y")
+    # pos = []
 
-    # ax.set_xlabel("X")
-    # ax.set_ylabel("Y")
-    # ax.set_zlabel("Z")
+    times = [0,2]
+    position = [0,10]
+    velocities = [0,0]
+
+    setInterpolate1d(position,velocities,times)
+
+    Xs = []
+    Ys=[]
+
+    def pathFun(theta):
+        stepLength = 10.0
+        Amplitude = 5.0
+        return ( np.sin(np.pi * theta / stepLength) * Amplitude)
+
+    start = time.time()
+    current = time.time()
+    while (time.time()-start <= ( Final_time-Iniital_time)):
+        config = getInterpolate1d( time.time()-start)
+        Xs.append(config)
+        Ys.append(pathFun(config))
+
+        # ax.scatter(1,1,1)
+        time.sleep(0.1)
+        print("Calculating at time:",time.time()-start)
+    
+    fig = plt.figure()
+    
+    plt.scatter(Xs,Ys)
+    # ax = fig.add_subplot(111, projection='3d')
     # # plt.ylabel("Time")
-    # plt.show()
+    plt.show()
 
 
 
