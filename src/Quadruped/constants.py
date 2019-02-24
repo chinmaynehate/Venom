f3502501acce726e4db207fb410057c85f4cc3be
swapnil <swapnilmane1393@gmail.com>
2019-02-24 14:54:30 +0530

    DXL_LIB_PATH

--- src/Quadruped/constants.py
+++ src/Quadruped/constants.py
@@ -2,6 +2,7 @@ ttyUSB_USB2DYNAMIXEL = "/dev/ttyUSB3"
 
 # DXL_LIB_PATH = "/home/carnage/DynamixelSDK/c/build/linux64/libdxl_x64_c.so"
 DXL_LIB_PATH = "/home/shanks/Downloads/Libraries/DynamixelSDK-master/c/build/linux64/libdxl_x64_c.so"
+DXL_LIB_PATH = "/home/swapnil/Documents/new/DynamixelSDK-master/c/build/linux64/libdxl_x64_c.so"
 
 
 ENABLE_DXL_MESSAGES = False
