b40bc97e29bb66fd8cfda425a12e612a76f0e5c2
SuyashMore <suyash.21.more@gmail.com>
2019-02-25 21:45:32 +0530

    fourier series graph to test

--- src/Quadruped/Core/FourierSeries.py
+++ src/Quadruped/Core/FourierSeries.py
@@ -1,4 +1,6 @@
-from sympy import fourier_series,symbols
+from sympy import fourier_series,symbols,Piecewise
+import matplotlib.pyplot as plt
+import numpy as np
 
 def constructTrajectory_FourierSeries():
     dc = 0.8
@@ -15,10 +17,24 @@ def constructTrajectory_FourierSeries():
 
     t_current = symbols('t_current')
 
-    fx1 = lambda t_current:(stroke/2.0 + stroke/dc*(-t_current))
-    fx2 = lambda t_current:a0 + a1*(t_current-(dc+dt)) + a2*(t_current-(dc+dt))**2.0 + a3*(t_current-(dc+dt))**3.0; #stride 
-    fx3 = lambda t_current:(stroke/2.0 + stroke/dc*(-t_current))
+    fx1 = (stroke/2.0 + stroke/dc*(-1* t_current))
+    fx2 = a0 + a1*(t_current-(dc+dt)) + a2*(t_current-(dc+dt))**2.0 + a3*(t_current-(dc+dt))**3.0; #stride 
+    fx3 = (stroke/2.0 + stroke/dc*(-1* t_current))
 
+    print("Calculating Piecewise Fourier Series")
+    FourierX = Piecewise( (fx1,t_current<=dc+dt) ,  (fx2 ,t_current<=T-dt ) , (fx3 , t_current<=1)   )
+
+    FourierXSeries = fourier_series( FourierX , (t_current,0,1)  )
+    print("FourierX Series : ", FourierXSeries)
+
+    lp = np.array(range( 0 , 100))
+    for i in lp:
+        current = FourierXSeries.subs(t_current,i/10)
+        plt.scatter( i/10 , current )
+
+    plt.show()
+
+    '''
     print("calculating Fourier1")
     Fourier1 = fourier_series( fx1(t_current) , (t_current,0,dc+dt)   )
     print("calculating Fourier2")
@@ -32,9 +48,10 @@ def constructTrajectory_FourierSeries():
     print("\n\n\n")
     print("Fourier3 = ",Fourier3.truncate())
 
+    '''
 
-    FourierTotal = Fourier1+Fourier2+Fourier3
-    print(FourierTotal)
+    # FourierTotal = Fourier1+Fourier2+Fourier3
+    # print(FourierTotal)
 
 
 
